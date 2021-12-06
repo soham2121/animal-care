0525adb775cc91ca3dbaa6e55ae326ea0c9d4fcb
GamingStudios21 <56083422+soham2121@users.noreply.github.com>
2021-12-06 18:04:57 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
 import LoginScreen from './screens/LoginScreen';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { AppDrawerNavigator } from './components/AppDrawerNavigator';
@@ -18,12 +17,3 @@ const switchnavigator = createSwitchNavigator({
 });
 
 const AppContainer = createAppContainer(switchnavigator)
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
